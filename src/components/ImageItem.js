19b67a1170cdeeeff5a51502f58fafd3fa30e42c
Elle Hallal <daniellehallal@gmail.com>
2019-02-23 23:39:43 +0000

    Image item accepts props id, likes, description, imageurl and downloadlink

--- src/components/ImageItem.js
+++ src/components/ImageItem.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
-const ImageItem = () => {
-    return (<div>Hi</div>);
+const ImageItem = (props) => {
+    return (
+        <div key={props.id}>
+            <div className="ui card">
+                <div className="content">
+                    {props.description}
+                </div>
+                <div className="image">
+                    <img alt={props.description} src={props.imageurl} data-test="image" />
+                </div>
+                <div className="content">
+                    <span className="right floated">
+                        <i className="heart outline like icon"></i>
+                        {props.likes} likes
+                    </span>
+                </div>
+                <div className="extra content">
+                    <a href={props.downloadLink} target="_blank" rel="noopener noreferrer" data-test="download-url">Download image</a>
+                </div>
+            </div>
+        </div >);
 }
 
 export default ImageItem;
--- src/components/__tests__/ImageItem.test.js
+++ src/components/__tests__/ImageItem.test.js
@@ -8,3 +8,18 @@ Enzyme.configure({ adapter: new EnzymeAdapter() });
 test("renders without crashing", () => {
     shallow(<ImageItem />);
 });
+
+test("accepts props and displays them", () => {
+    const wrapper = shallow(<ImageItem id="22222" likes="100" description="a black cat" imageurl="https://test.com" linksDownload="https://test.com/dl" />);
+
+    const image = wrapper.find(`[data-test='image']`);
+    console.log(image)
+    const download = wrapper.find(`[data-test='download-url']`);
+
+    expect(wrapper.text()).toContain("100");
+    expect(wrapper.text()).toContain("a black cat");
+    expect(image.prop('src')).toEqual('https://test.com');
+    expect(image.prop('alt')).toEqual('a black cat');
+    expect(download.prop('href')).toEqual('https://test.com/dl');
+
+});
